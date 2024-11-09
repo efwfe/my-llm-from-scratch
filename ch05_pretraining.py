3c8128b2a1a763360009a3f54707f2cc252aabb9
david <471404259@qq.com>
2024-11-09 15:37:49 +0800

    add pretaining and gpt loading

--- ch05_pretraining.py
+++ ch05_pretraining.py
@@ -129,4 +129,530 @@ train_data = text_data[:split_idx]
 val_data = text_data[split_idx:]
 
 # %%
-from ch0
+from ch02_tokenizer import create_dataloader_v1
+
+torch.manual_seed(123)
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M['context_length'],
+    stride=GPT_CONFIG_124M['context_length'],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M['context_length'],
+    stride=GPT_CONFIG_124M['context_length'],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+# %%
+print("Train")
+
+for x, y in train_loader:
+    print(x.shape, y.shape)
+# %%
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1),
+        target_batch.flatten()
+    )
+    return loss
+# %%
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float('nan')
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(len(data_loader), num_batches)
+    
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch,
+                target_batch,
+                model,
+                device
+            )
+
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+
+
+# %%
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
+model.to(device)
+
+with torch.no_grad():
+    train_loss = calc_loss_loader(train_loader, model, device)
+    val_loss = calc_loss_loader(val_loader, model, device)
+
+print("Train loss",train_loss)
+print("Val loss", val_loss)
+# %%
+
+def evaluate_model(model, train_lodaer, val_loader ,device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_lodaer, model, device, num_batches=eval_iter
+        )
+
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+
+    model.train()
+    return train_loss, val_loss
+
+
+
+def generate_and_print_sample(
+    model, tokenizer, device, start_context
+):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model,
+            idx=encoded,
+            max_new_tokens=50,
+            context_size=context_size
+        )
+
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", ""))
+
+    model.train()
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, 
+                        eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+
+            loss.backward()
+            optimizer.step()
+
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model,
+                    train_loader,
+                    val_loader,
+                    device, 
+                    eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+
+                print(f"Ep {epoch + 1} Step {global_step:06d}:"
+                    f"Train loss {train_loss:.3f},"
+                    f"Val loss {val_loss:.3f}"
+                )
+            
+            generate_and_print_sample(
+                model, tokenizer, device, start_context
+            )
+    return train_losses, val_losses, track_tokens_seen
+
+
+
+
+# %%
+# ADAMW
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+
+model.to(device)
+
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=0.0004,
+    weight_decay=0.1
+)
+
+num_epochs = 10
+
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+    start_context="Every effort moves you",
+    tokenizer=tokenizer
+)
+# %%
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5,3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen ,val_losses, linestyle='-', label="Validation loss")
+
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
+
+
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+# %%
+# Decoding stategies to control randomness
+
+# temperatruie scaling and top-k sampling 
+
+model.to("cpu")
+model.eval()
+
+# %%
+tokenzier = tiktoken.get_encoding("gpt2")
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=25,
+    context_size=GPT_CONFIG_124M['context_length']
+)
+
+print(f"Output text: \n {token_ids_to_text(token_ids, tokenizer)}")
+# %%
+
+# Temperatur scaling
+
+vocab = { 
+    "closer": 0,
+    "every": 1, 
+    "effort": 2, 
+    "forward": 3,
+    "inches": 4,
+    "moves": 5, 
+    "pizza": 6,
+    "toward": 7,
+    "you": 8,
+} 
+
+inverse_vocab = {v: k for k, v in vocab.items()}
+
+
+# %%
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+next_token_id = torch.argmax(probas).item()
+
+print(inverse_vocab[next_token_id])
+# %%
+
+torch.manual_seed(123)
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])
+# %%
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123)
+    sample = [
+        torch.multinomial(probas, num_samples=1).item()
+        for i in range(1000)
+    ]
+
+    sampled_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq}x{inverse_vocab[i]}")
+    
+print_sampled_tokens(probas)
+# %%
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+# %%
+temperature = [1, 0.1, 5]
+
+scaled_probas = [
+    softmax_with_temperature(next_token_logits, T)
+    for T in temperature
+]
+
+x = torch.arange(len(vocab))
+bar_width = 0.15
+fig, ax = plt.subplots(figsize=(5,3))
+for i, T in enumerate(temperature):
+    rects = ax.bar(x + i * bar_width, scaled_probas[i], bar_width, label=f"Temperature={T}")
+
+ax.set_ylabel("Probability")
+ax.set_xticks(x)
+ax.set_xticklabels(vocab.keys(), rotation=90)
+ax.legend()
+plt.tight_layout()
+plt.show()
+# %% TOP-K sampling
+
+# using top-k sampling with k=3, we focus on the trhee tokens associated with the highest logits and mask out all other tokens with -inf befor 
+# applying the softmax function.
+
+
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print(f"Top logits, {top_logits}")
+print(f"Top positions: {top_pos}")
+# %%
+new_logts = torch.where(
+    condition=next_token_logits < top_logits[-1],
+    input=torch.tensor(float('-inf')),
+    other=next_token_logits
+)
+print(new_logts)
+# %%
+top_k_probas = torch.softmax(new_logts,  dim=0)
+print(top_k_probas)
+# %%
+
+def generate(model, idx, max_new_tokens, context_size, temperature=0., top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        logits = logits[:, -1, :]
+
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                condition=logits < min_val,
+                input = torch.tensor(float('-inf')).to(logits.device),
+                other=logits
+            )
+        
+        if temperature > 0.:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.softmax(logits, dim=-1, keepdim=True)
+
+        if idx_next == eos_id:
+            break
+
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+
+# %%
+torch.manual_seed(123)
+token_ids = generate(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=15,
+    context_size = GPT_CONFIG_124M['context_length'],
+    top_k=25,
+    temperature=5)
+print(f"Output text: {token_ids_to_text(token_ids, tokenizer)}")
+
+
+# %% Loading and Saving model weights
+torch.save(model.state_dict(), "model.pth")
+
+# %%
+model = GPTModel(GPT_CONFIG_124M)
+model.load_state_dict(torch.load('model.pth', map_location=device))
+model.eval()
+# %%
+# 
+torch.save(
+    {
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict()
+    },
+    "model_and_optimizer.pth"
+)
+# %%
+# restore to train
+
+checkpoint = torch.load("model_and_optimizer.pth", map_location=device)
+model = GPTModel(GPT_CONFIG_124M)
+model.load_state_dict(checkpoint['model_state_dict'])
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-4, weight_decay=0.1)
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+model.train()
+# %%
+
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs=1, eval_freq=5, eval_iter=5,
+    start_context="Every effort moves you",
+    tokenizer=tokenizer
+)
+# %%
+# Loading pretrained weights from OpenAI
+
+# %%
+import urllib.request
+url = (
+    "https://raw.githubusercontent.com/rasbt/"
+    "LLMs-from-scratch/main/ch05/"
+    "01_main-chapter-code/gpt_download.py"
+)
+filename = url.split('/')[-1]
+urllib.request.urlretrieve(url, filename)
+# %%
+from gpt_download import download_and_load_gpt2
+settings, params = download_and_load_gpt2(
+    model_size="124M", models_dir="gpt2"
+)
+# %%
+print(settings, params.keys())
+
+# %%
+print(params['wte'])
+print(f"Token embedding wieght tensor dimensions: {params['wte'].shape}")
+# %%
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+# %%
+model_name = "gpt2-small (124M)"
+NEW_CONFIG = GPT_CONFIG_124M.copy()
+NEW_CONFIG.update(model_configs[model_name])
+# %%
+NEW_CONFIG.update({"context_length": 1024})
+# %%
+NEW_CONFIG.update({"qkv_bias": True})
+# %%
+gpt = GPTModel(NEW_CONFIG)
+gpt.eval()
+# %%
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left:{left.shape}, Right:{right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
+# %%
+import numpy as np
+
+def load_weiths_into_gpt(gpt, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+
+    for b in range(len(params["blocks"])):     #2
+        q_w, k_w, v_w = np.split(                            #3
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = assign(
+            gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(
+            gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(
+            gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(
+            gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(
+            gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(
+            gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight, 
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias, 
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight, 
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias, 
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight, 
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias, 
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale, 
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift, 
+            params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale, 
+            params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift, 
+            params["blocks"][b]["ln_2"]["b"])
+
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"]) 
+# %%
+
+load_weiths_into_gpt(gpt, params)
+
+# %%
+gpt.to(device)
+# %%
+torch.manual_seed(123)
+token_ids = generate(
+    model=gpt,
+    idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
+    max_new_tokens=25,
+    context_size=NEW_CONFIG['context_length'],
+    top_k=50,
+    temperature=1.5
+)
+
+print(f"Output: {token_ids_to_text(token_ids, tokenizer)}")
+# %%
