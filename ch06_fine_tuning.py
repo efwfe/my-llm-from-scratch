0f3e12bf3abb17b3fe58f62acac937d1a2532fed
david <471404259@qq.com>
2024-11-10 22:32:32 +0800

    add ch06

---
+++ ch06_fine_tuning.py
@@ -0,0 +1,7 @@
+# %% 
+# 1. llm ft approches
+# 2. preparate dataset for text classification
+# 3. modify pretrained llm for ft
+#  4. evaluate the accuracy of llm classifier
+#  5. a fine-tuned llm to classify new data 
+import torch
